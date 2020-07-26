113ac32f6fd013178055d33339558796f51ab92b
kunal swami <kunalswami29@gmail.com>
2020-07-27 00:19:37 +0530

    a wheel and screen coponent is addded

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Wheel from './Wheel';
+import Screen from './Screen'
+
+function App() {
+  return (
+    <div className="app-container">
+       <Screen/>
+       <Wheel/>
+    </div>
+    
+  )
+}
+
+export default App;
